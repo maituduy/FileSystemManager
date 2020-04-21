6aa9e87aad4605f42ed576719603c3a6368034ae
mxw <maituduy1998@gmail.com>
2020-04-21 20:44:12 +0700

    add more tests

--- FilesystemManager.cpp
+++ FilesystemManager.cpp
@@ -10,90 +10,90 @@ namespace fsmanager {
 
     void viewFolder(std::string path, view_option option) {
 
-        for (const auto &entry : fs::directory_iterator(path)) {
-            std::cout << entry.path() << std::endl;
+      for (const auto &entry : fs::directory_iterator(path)) {
+        std::cout << entry.path() << std::endl;
 
-            if (option == view_option::all)
-                if (fs::is_directory(entry.path()))
-                    viewFolder(entry.path(), option);
-        }
+        if (option == view_option::all)
+          if (fs::is_directory(entry.path()))
+            viewFolder(entry.path(), option);
+      }
     }
 
     bool createFolder(std::string path) {
-        return fs::create_directories(path);
+      return fs::create_directories(path);
     }
 
     bool renameFolder(std::string oldname, std::string newname) {
-        if (!fs::is_directory(oldname))
-            return false;
+      if (!fs::is_directory(oldname))
+        return false;
 
-        std::error_code errorCode;
-        fs::rename(oldname, newname, errorCode);
-        return !errorCode.value();
+      std::error_code errorCode;
+      fs::rename(oldname, newname, errorCode);
+      return !errorCode.value();
     }
 
     bool copyFolder(std::string path_from, std::string path_to) {
-        if (!fs::is_directory(path_from))
-            return false;
+      if (!fs::is_directory(path_from))
+        return false;
 
-        std::error_code errorCode;
-        fs::copy(path_from, path_to, fs::copy_options::recursive, errorCode);
-        return !errorCode.value();
+      std::error_code errorCode;
+      fs::copy(path_from, path_to, fs::copy_options::recursive, errorCode);
+      return !errorCode.value();
     }
 
     bool findFolder(std::string path) {
-        return fs::is_directory(path);
+      return fs::is_directory(path);
     }
 
     bool moveFolder(std::string path_from, std::string path_to) {
-        return renameFolder(path_from, fs::path(path_to) / fs::path(path_from).filename());
+      return renameFolder(path_from, fs::path(path_to) / fs::path(path_from).filename());
     }
 
     bool deleteFolder(std::string path) {
-        return fs::is_directory(path) & fs::remove_all(path);
+      return fs::is_directory(path) & fs::remove_all(path);
     }
 
     bool writeFile(std::string path, const void *buf, __int64_t buflen) {
 
-        std::ofstream ofs(path, std::ofstream::binary);
-        if (ofs) {
-            ofs.write(static_cast<const char *>(buf), buflen);
-            ofs.close();
-            return true;
-        }
-        return false;
+      std::ofstream ofs(path, std::ofstream::binary);
+      if (ofs) {
+        ofs.write(static_cast<const char *>(buf), buflen);
+        ofs.close();
+        return true;
+      }
+      return false;
     }
 
     bool renameFile(std::string oldname, std::string newname) {
-        if (!fs::is_regular_file(oldname))
-            return false;
+      if (!fs::is_regular_file(oldname))
+        return false;
 
-        std::error_code errorCode;
-        fs::rename(oldname, newname, errorCode);
-        return !errorCode.value();
+      std::error_code errorCode;
+      fs::rename(oldname, newname, errorCode);
+      return !errorCode.value();
     }
 
     bool copyFile(std::string path_from, std::string path_to) {
-        if (!fs::is_regular_file(path_from))
-            return false;
+      if (!fs::is_regular_file(path_from))
+        return false;
 
-        std::error_code errorCode;
-        fs::copy(path_from, path_to, errorCode);
-        return !errorCode.value();
+      std::error_code errorCode;
+      fs::copy(path_from, path_to, errorCode);
+      return !errorCode.value();
     }
 
     bool findFile(std::string path) {
-        return fs::is_regular_file(path);
+      return fs::is_regular_file(path);
     }
 
     bool moveFile(std::string path_from, std::string path_to) {
-        return renameFile(path_from, path_to);
+      return renameFile(path_from, fs::path(path_to) / fs::path(path_from).filename());
     }
 
     bool deleteFile(std::string path) {
-        if (fs::is_regular_file(path))
-            return fs::remove(path);
-        return false;
+      if (fs::is_regular_file(path))
+        return fs::remove(path);
+      return false;
     }
 
 
--- TestFilesystemManager.cpp
+++ TestFilesystemManager.cpp
@@ -1,67 +1,111 @@
 #include <iostream>
 #include <fstream>
 #include "FilesystemManager.h"
-
+#include <chrono>
 #include <experimental/filesystem>
+
 namespace fs = std::experimental::filesystem;
 fs::path temp_path = fs::temp_directory_path();
 
 void test_create_folder() {
-    fsmanager::createFolder(temp_path/"root/a/b/c");
-    fsmanager::createFolder(temp_path/"root/d/e");
+  fsmanager::createFolder(temp_path / "root/a/b/c");
+  fsmanager::createFolder(temp_path / "root/d/e");
 }
 
 void test_rename_folder() {
-    fsmanager::renameFolder(temp_path/"root/a", temp_path/"root/new_a");
-    fsmanager::renameFolder(temp_path/"root/d/e", temp_path/"root/d/new_e");
+  fsmanager::renameFolder(temp_path / "root/a", temp_path / "root/new_a");
+  fsmanager::renameFolder(temp_path / "root/d/e", temp_path / "root/d/new_e");
 }
 
 void test_copy_folder() {
-    fsmanager::copyFolder(temp_path/"root/new_a", temp_path/"root/copy_a");
+  fsmanager::copyFolder(temp_path / "root/new_a", temp_path / "root/copy_a");
 }
 
 void test_move_folder() {
-    fsmanager::moveFolder(temp_path/"root/d/new_e", temp_path/"root/new_a");
+  fsmanager::moveFolder(temp_path / "root/d/new_e", temp_path / "root/new_a");
 }
 
 void test_find_folder() {
-    std::cout << fsmanager::findFolder(temp_path/"root/copy_a/b/c") << std::endl;
-    std::cout << fsmanager::findFolder(temp_path/"not_exist") << std::endl;
+  std::cout << fsmanager::findFolder(temp_path / "root/copy_a/b/c") << std::endl;
+  std::cout << fsmanager::findFolder(temp_path / "not_exist") << std::endl;
 }
 
 void test_view_folder() {
-    fsmanager::viewFolder(temp_path/"root", fsmanager::view_option::all);
+  fsmanager::viewFolder(temp_path / "root", fsmanager::view_option::all);
 }
 
 void test_write_file() {
-    std::string test1 = "Test1";
-    const char* c1 = test1.c_str();
-    const auto str_size = test1.size();
-    fsmanager::writeFile(temp_path/"root/d/test1.txt", c1, str_size);
+  std::string test1 = "Test1";
+  const char *c1 = test1.c_str();
+  const auto str_size = test1.size();
+  fsmanager::writeFile(temp_path / "root/d/test1.txt", c1, str_size);
 
-    // wirte zip file
-    std::ifstream infile ("3/tk.pdf",std::ifstream::binary);
-    infile.seekg (0, infile.end);
-    long size = infile.tellg();
-    infile.seekg (0);
+  // wirte zip file
+//  std::ifstream infile("myzip.zip", std::ifstream::binary);
+//  infile.seekg(0, infile.end);
+//  long size = infile.tellg();
+//  infile.seekg(0);
+//
+//  // allocate memory for file content
+//  char *buffer = new char[size];
+//
+//  // read content of infile
+//  infile.read(buffer, size);
+//  std::chrono::time_point<std::chrono::system_clock> start, end;
+//  start = std::chrono::system_clock::now();
+//  fsmanager::writeFile(temp_path / "root/d/test2.zip", buffer, size);
+//  end = std::chrono::system_clock::now();
+//
+//  std::chrono::duration<double> elapsed_seconds = end - start;
+//  std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+//
+//  std::cout << "finished computation at " << std::ctime(&end_time)
+//            << "elapsed time: " << elapsed_seconds.count() << "s\n";
+//  infile.close();
+}
 
-    // allocate memory for file content
-    char* buffer = new char[size];
+void test_rename_file() {
+  fsmanager::renameFile(temp_path / "root/d/test1.txt", temp_path / "root/d/test_new.txt");
+}
 
-    // read content of infile
-    infile.read (buffer,size);
+void test_copy_file() {
+  fsmanager::copyFile(temp_path / "root/d/test_new.txt", temp_path / "root/new_a/new_e");
+}
+
+void test_find_file() {
+  bool ok = fsmanager::findFile(temp_path / "root/new_a/new_e/test_new.txt");
+  if (ok)
+    std::cout << temp_path / "root/new_a/new_e/test_new.txt" << " existed\n";
+  else
+    std::cout << temp_path / "root/new_a/new_e/test_new.txt" << " not exist\n";
+
+}
+
+void test_move_file() {
+  fsmanager::moveFile(temp_path / "root/d/test_new.txt", temp_path / "root/new_a/b");
+}
+
+void test_delete_file() {
+  fsmanager::deleteFile(temp_path / "root/new_a/new_e/test_new.txt");
 }
 
 int main() {
 
-    test_create_folder();
-    test_rename_folder();
-    test_copy_folder();
-    test_move_folder();
-    test_write_file();
+  test_create_folder();
+  test_rename_folder();
+  test_copy_folder();
+  test_move_folder();
 
-    test_view_folder();
-    test_find_folder();
-    fsmanager::deleteFolder(temp_path/"root");
-    return 0;
+  test_write_file();
+  test_rename_file();
+  test_copy_file();
+  test_move_file();
+  test_delete_file();
+
+  test_view_folder();
+  test_find_folder();
+  test_find_file();
+
+  fsmanager::deleteFolder(temp_path / "root");
+  return 0;
 }
